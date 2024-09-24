b32c0a5529cd4c77fffcb775c186670e15739d1a
sumdx <sumdas74@gmail.com>
2024-09-23 19:11:45 -0700

    frontend design done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          navBg: '#F9F7F3',
+          btnBg: '#B4F461',
+        },
+      },
+    },
+    plugins: [],
+  }
+  
